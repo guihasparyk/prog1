c8c4d017a678e36f6bd84dc9a8674836f11c94e2
guihasparyk <50344202+guihasparyk@users.noreply.github.com>
2020-12-09 20:00:52 +0000

    Update TT1.java
    q(a) has to be before 'for (int i)', unless not working

--- EstTeste/TT1.java
+++ EstTeste/TT1.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 
-class TT1{
-    public static final Scanner KB = new Scanner(System.in);
+public class TT1 {
     
     public static void main(String[] args) {
         int[] a = {1,12,5,4};
         q(a);
-        for(int i=a.length-1;i>=0;i--)System.out.printf("%d ",a[i]);
+        System.out.print("\n");
+        for(int i = a.length - 1; i >= 0; i--) System.out.printf("%d ", a[i]);
     }
-
-    public static void q(int[] p){
-        for(int i=0;i<p.length/2;i++){
+    public static void q(int[] p) {
+        for(int i = 0; i < p.length / 2 ;i++) {
+			
             int tmp = p[i];
-            p[i] = p[p.length-1-i];
-            p[p.length-1-i] = tmp;
+            
+            p[i] = p[p.length - 1 - i];
+            
+            p[p.length - 1 - i] = tmp;
         }
+        for (int i = 0; i < p.length; i++) System.out.printf("%d ", p[i]);
     }
 }
